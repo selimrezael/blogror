0f36d5326e9b130bf6a941f4d26260d43082d7e3
Selim Reza <selimrezael@gmail.com>
2016-07-26 15:45:42 +0600

    fixed name input while signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
+  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"},
+             controller: { registrations: 'registrations' }
 
   resources :posts do
     resources :comments
