4ff026ff67de1a62aa45eb5ef56a3062ed818399
Selim Reza <selimrezael@gmail.com>
2016-07-26 15:50:17 +0600

    removed unnecessary registration controller from routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"},
-             controller: { registrations: 'registrations' }
+  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
 
   resources :posts do
     resources :comments
